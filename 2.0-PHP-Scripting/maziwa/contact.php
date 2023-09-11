e40c7bca94d9f0263184a9964a2c7eaa3fcdb95e
Tervilgachiri <george.gachiri@strathmore.edu>
2023-09-11 09:49:38 +0300

    First Steps in PHP

---
+++ 2.0-PHP-Scripting/maziwa/contact.php
@@ -0,0 +1,39 @@
+<!--HTML-->
+<h1>My first PHP file</h1>
+<head>
+     <style>
+        /* CSS */
+        p{
+            color : blue;
+            font-size: large;
+        }
+    </style>
+    <script>
+
+        /* JAVASCRIPT */
+        alert('checking everything out');
+        </script>
+</head>
+
+
+<?php // opening tags
+
+      //php code goes here
+      echo "<p>From PHP</P>";
+      echo "<p>From PHP</P>";
+      echo "<p>From PHP</P>";
+      print"<p>From PHP using print</P>";
+      
+      //reserved words /functions are CASE insensitive
+
+      //@TODO #2 : difference between echo and print
+
+
+
+
+
+//closing tags
+
+?>
+
+<p></p>
