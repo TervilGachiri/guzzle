999e1023b11b3637eb8077eb222385176024a5b4
Tervilgachiri <george.gachiri@strathmore.edu>
2023-09-12 20:52:59 +0300

    6th step in PHP

--- 2.0-PHP-Scripting/maziwa/process.php
+++ 2.0-PHP-Scripting/maziwa/process.php
@@ -10,73 +10,81 @@
  * -Return a success message to the user
  */
 
- //1.Get the data
- //there is some data
- //it is a POST instead of a GET
+//1.Get the data
+//there is some data
+//it is a POST instead of a GET
 
- //@TODO #5 : Data and input
- //@TODO #6 : Validation
- //@TODO #7 : Send back messages
- 
-
- //superglobals, predefined/Built-in-variables
+//@TODO #5 : Data and input
+//@TODO #6 : Validation
+//@TODO #7 : Send back messages
 
 
- if( isset($_POST['submit']) && $_POST['submit'] == "submit"){
-    //VARIABLES 101 $_POST,$_GET,$_SERVER...
+//superglobals, predefined/Built-in-variables
+
+
+if (isset($_POST['submit']) && $_POST['submit'] == "submit") {
+   //VARIABLES 101 $_POST,$_GET,$_SERVER...
    // var_dump($_POST); // variables are case sensitive _POSt  _POST
-    //User defined variable
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $country = $_POST['country'];
-    $pcode = $_POST['pcode'];
-    $pnumber = $_POST['pnumber'];
-    $phone = $pcode.$pnumber;
-    $today = date("Y-m-d H:i:s"); //2023.09.11 12:27:37//gives us my sql datetime format for today/now
+   //User defined variable
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+   $country = $_POST['country'];
+   $pcode = $_POST['pcode'];
+   $pnumber = $_POST['pnumber'];
+   $phone = $pcode . $pnumber;
+   $today = date("Y-m-d H:i:s"); //2023.09.11 12:27:37//gives us my sql datetime format for today/now
 
-    //2.Validation
-    //make sure the format of what we expect is adhered to
-    //for example , a name should be a name ie.there are no names that start with numbers
-    $name_pattern = '/[a-zA-Z \']{3,30}$/';
-    if(preg_match($name_pattern,$name)){
-        echo "valid";
-    }else{
-      echo "invalid";
-    }
+   //2.Validation
+   //make sure that the format of what we expect is cadhered to
+   //for example, a name should be a name i.e.there are no names that start with numbers
+   $name_pattern = '/[a-zA-z \']{3,30}$/';
+   if (preg_match($name_pattern, $name)) {
+      //3.store the data in a database table
+      //3.1 make a database connection
+      $db = mysqli_connect('localhost', 'api_user', 'api_user', 'class_db');
+
+      $db = mysqli_connect('localhost', 'root', '', 'class_db');
+
+      //if(mysqli_connect_error () ){
+      //    // String operation - joining of strings or concatenation .(dot opearator)
+      //    die("Database connection failed!" .mysqli_connect_error () );
+      //    //stop execution here!
+      //}
+
+      //@TODO : Read on sql injection
+      //SQL injection is a code injection technique that might destroy your database. 
+      //escaping
+      $name = mysqli_real_escape_string($db,$name);
+      $message = mysqli_real_escape_string($db,$message);
+
+      //encode html entities
+      $message = htmlentities($message);   
+
+      #sql = "INSERT INTO contact('name', 'email','country','contact_date','phone','message') 
+  VALUES('$name', '$email', '$country', '$today', '$phone', '$message');
+
+        //echo $sql;/
    
-    die('stopped execution');
+//die();
 
-    
-    //3.store the data in a database table
-    //3.1 make a database connection
-    $db = mysqli_connect('localhost','api_user','api_user','class_db');
-
-    $db=mysqli_connect('localhost','root','','class_db');
-
-    //if(mysqli_connect_error () ){
-    //    // String operation - joining of strings or concatenation .(dot opearator)
-    //    die("Database connection failed!" .mysqli_connect_error () );
-    //    //stop execution here!
-    //}
-
-    #sql = "INSERT INTO contact('name', 'email','country','contact_date','phone','message') 
-    VALUES('$name','$email','$country','$today','$phone','$message');
-
-    //$string1 = "Nganga";
-    //$string1 = " John said ,"I tell you" ';
-    //$string1 =Ng'ang'a";
-    //$string1 =Ng\'ang\"a";
+      // $string1 = "Nganga";
+      // $string1 = " John said ,"I tell you" ';
+      // $string1 = Ng'ang'a";
+      // $string1 = Ng\'ang\"a";
 
 
-    //insert data
-    mysqli_query($db,$sql);
+      //insert data
+      mysqli_query($db, $sql);
 
-    //4.Return a success mssage to the user
-    echo "Your message has been received";
-    echo "<a href='index.php'>Go To Home Page</a>";
- }else{
-    //redirect the user back to the home page
-    header('Location:index.php'); 
-
- }
+      //4.Return a success mssage to the user
+      echo "Your message has been received";
+      echo "<a href='index.php'>Go To Home Page</a>";
+   } else {
+      echo "Invalid name provided :<button onclick='history.back()'>Go  Back to contact Page</button>";
+      die();
+   }
+} else {
+   //redirect the user back to the home page
+   header('Location:index.php');
+}
