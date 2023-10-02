4d690db81ac62f179bd7bbfb4c748ae5a070f34a
Tervilgachiri <george.gachiri@strathmore.edu>
2023-10-02 20:52:49 +0300

    PHP-OOP-Autoloading Classes

---
+++ 3.0-OOP-PHP/maziwa/User/Staff.php
@@ -0,0 +1,44 @@
+<?php
+namespace  User;
+
+//include our class loader
+require_once '../Classloader.php';
+
+
+//aliasing 
+use \Database\Database as Database;
+
+class Staff{
+
+    protected $id;
+    protected $lastname;
+    protected $firstname;
+    protected $fullname;
+    protected $shortBio;
+    protected $emailAddress;
+    protected $role;
+    protected $employmentDate;
+    protected $status;
+
+    public function __construct(){
+        //get the database connection
+        $db = new \Database\Database();
+    }
+}
+
+//@TODO #15:  Advantages of PHP Autoloading
+//Reduces Redundancy and Boilerplate Code
+//Promotes Scalability
+//Improves Development Efficiency
+//Simplifies Code Maintenance
+
+//@TODO #14: Why do we need to autoload files in PHP
+//Efficiency and Performance
+//Saves Development Time
+//Promotes Scalabilit
+//Simplifies Maintenance
+//Encourages Best Practices
+
+//an object named person of type Staff
+//a Staff object named person 
+$person = new Staff();
